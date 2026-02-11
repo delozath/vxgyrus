6ea72c3a9b15ae7a6e4f2898a08ed61ded037942
delozath <delozath@gmail.com>
2026-02-11 14:11:40 -0600

    feat: Add VTK rendering to the main window works

--- src/vxgyrus/presentation/qt/actions.py
+++ src/vxgyrus/presentation/qt/actions.py
@@ -4,15 +4,17 @@ from pathlib import Path
 
 from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
+from PyQt5 import QtWidgets
+
 class ActionsPyQt5:
-    def __init__(self, win):
-        self.host = win
+    def __init__(self, host):
+        self.host = host
         self._connect()
     
     def on_open(self, path):
         ext = Path(path).suffix
         #print(f"Selected file: {path}")
-        data = self._loaders[ext]().load(self, path)
+        data = self._loaders[ext]().load(self.host, path)
         #print(f"Data loaded: {data}")
         return data
     
@@ -23,31 +25,42 @@ class ActionsPyQt5:
             ".jpeg": ActionsVTK
         }
 
+
 class ActionsVTK:
     def load(self, host, path: str):
-        reader = vtk.vtkImageReader2Factory().CreateImageReader2(path)
-        reader.SetFileName(path)
-        reader.Update()
+        render = vtk.vtkImageReader2Factory().CreateImageReader2(path)
+        render.SetFileName(path)
+        render.Update()
+    
+        container = host.findChild(QtWidgets.QWidget, "widgetMain2DViewer")
+        layout = container.layout()
+        if layout is None:
+            layout = QtWidgets.QVBoxLayout(container)
+            layout.setContentsMargins(0, 0, 0, 0)
+            layout.setSpacing(0)
 
-        layout = host.host.layout()
-        print(f"{layout}")
-
-        self.vtk_widget = QVTKRenderWindowInteractor(host.host)
-        layout.addWidget(self.vtk_widget)
-
-        self.rw = self.vtk_widget.GetRenderWindow()
-        self.ren = vtk.vtkRenderer()
-        self.rw.AddRenderer(self.ren)
-
-        self.actor = vtk.vtkImageActor()
-        self.ren.AddActor(self.actor)
-
-        self.vtk_widget.Initialize()
-
-        self.actor.GetMapper().SetInputConnection(reader.GetOutputPort())
+        # limpiar el contenedor (para no apilar widgets)
+        while layout.count():
+            item = layout.takeAt(0)
+            w = item.widget()
+            if w:
+                w.setParent(None)
+                w.deleteLater()
         
-        self.ren.ResetCamera()
-        self.rw.Render()
+        vtk_widget = QVTKRenderWindowInteractor(host)
+        layout.addWidget(vtk_widget)
+
+        rw = vtk_widget.GetRenderWindow()
+        ren = vtk.vtkRenderer()
+        rw.AddRenderer(ren)
+        actor = vtk.vtkImageActor()
+        ren.AddActor(actor)
+        vtk_widget.Initialize()
+        
+        actor.GetMapper().SetInputData(render.GetOutput())
+        ren.ResetCamera()
+        vtk_widget.GetRenderWindow().Render()
+        return render.GetOutput()
 
 
 class Temp():
--- src/vxgyrus/presentation/qt/main_window.py
+++ src/vxgyrus/presentation/qt/main_window.py
@@ -7,14 +7,5 @@ class App(QtWidgets.QMainWindow):
     def __init__(self, ui_path: str):
         super().__init__()
         uic.loadUi(ui_path, self) 
-
-        #self.host = self.findChild(QtWidgets.QWidget, "widgetMain2DViewer")
-        self.host = self.findChild(QtWidgets.QWidget, "widget")
-
-        layout = self.host.layout()
-        if layout is None:
-            layout = QtWidgets.QVBoxLayout(self.host)
-            layout.setContentsMargins(0, 0, 0, 0)
-            layout.setSpacing(0)
         
         self.dialogs = DialogsPyQt5(self)
